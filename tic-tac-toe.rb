142bee28456b09cda4b7544512650329f42a32f2
Stefano Rondelli <rstefanofil@hotmail.com>
2022-04-03 16:50:44 +0200

    Finish game
    I got a bit carried away and forgot to update as I went along.
    The game can now understand whether someone wins or the game ends
    in a tie, and can ask for a rematch.

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,5 +1,5 @@
 class Player
-    attr_accessor :name, :symbol
+    attr_reader :name, :symbol
     def initialize(name, symbol)
         @name = name
         @symbol = symbol
@@ -16,34 +16,57 @@ def play_game(grid, tokens, player_one, player_two)
         end
         tokens += 1
     end
+    puts "Unfortunately, the game was a tie."
 end
 
 def play_round(grid, tokens, player)
-        position_grid = choose_spot(grid, player)
-        grid[position_grid] = player.symbol
+    grid[choose_spot(grid, player)] = player.symbol
     display_grid(grid)
+    check_game_state(grid, player)
 end
 
 def choose_spot(grid, player)
-    
-    puts player.name + ", where do you want to place your token?"
+    puts "#{player.name}, where do you want to place your token?"
     position = gets.chomp.to_i
     until grid.index(position) != nil do
         puts "Invalid position, please enter a valid spot."
         position = gets.chomp.to_i
     end
-    position_grid = grid.index(position)    
+    grid.index(position)    
+end
+
+def check_game_state(grid, player)
+    if ((grid.values_at(0, 1, 2).all?(player.symbol)) || (grid.values_at(0, 3, 6).all?(player.symbol)) ||
+        (grid.values_at(1, 4, 7).all?(player.symbol)) || (grid.values_at(2, 5, 8).all?(player.symbol)) ||
+        (grid.values_at(3, 4, 5).all?(player.symbol)) || (grid.values_at(6, 7, 8).all?(player.symbol)) ||
+        (grid.values_at(0, 4, 8).all?(player.symbol)) || (grid.values_at(2, 4, 6).all?(player.symbol)))
+        puts "Congratulations #{player.name}, you won!\nWould you like to play another round?\nType y for yes and n for no."
+        answer = gets.chomp
+        if answer == "y"
+            start_game()
+        elsif answer == "n"
+            exit
+        end
+    end
 end
 
 def players_identity()
-    puts "Player 1, enter your name"
+    puts "Time for a nice game of good ol\' TicTacToe!\nPlayer 1, enter your name."
     name1 = gets.chomp
-    puts "Hello #{name1}, choose a symbol to represent you on the board"
+    puts "Hello #{name1}, choose a symbol to represent you on the board."
     symbol1 = gets.chomp
-    puts "Player 2, enter your name"
+    puts "Player 2, enter your name."
     name2 = gets.chomp
-    puts "Hello #{name2}, choose a symbol to represent you on the board"
+    until name2 != name1 do
+        puts "Invalid name, please choose a different one."
+        name2 = gets.chomp
+    end
+    puts "Hello #{name2}, choose a symbol to represent you on the board."
     symbol2 = gets.chomp
+    until symbol2 != symbol1 do
+        puts "Invalid symbol, please choose a different one."
+        symbol2 = gets.chomp
+    end
     return name1, symbol1, name2, symbol2
 end
 
@@ -51,12 +74,15 @@ def display_grid(grid)
     puts grid.slice(0, 3).join(" | ") + "\n_________\n\n" + grid.slice(3, 3).join(" | ") + "\n_________\n\n" + grid.slice(6, 3).join(" | ")
 end
 
-grid = Array.new(9) { | i | i + 1}
-tokens = 0
-display_grid(grid)
-# name1, symbol1, name2, symbol2 = players_identity()
-# player_one = Player.new(name1, symbol1)
-# player_two = Player.new(name2, symbol2)
-player_one = Player.new("Frodo", "F")
-player_two = Player.new("Sam", "S")
-play_game(grid, tokens, player_one, player_two)
+def start_game()
+    grid = Array.new(9) { | i | i + 1}
+    tokens = 0
+
+    name1, symbol1, name2, symbol2 = players_identity()
+    player_one = Player.new(name1, symbol1)
+    player_two = Player.new(name2, symbol2)
+    display_grid(grid)
+    play_game(grid, tokens, player_one, player_two)
+end
+
+start_game()
